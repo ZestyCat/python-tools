95ea05c431aa59646851ba300df1575c718e4395
Greg Bizup <g.bizup@gmail.com>
2022-04-18 10:58:55 -0700

    Added help button, option to save csv data, split plot into preview and save function, got rid of filename entry in favor of file dialog

--- noiseplot_generator/plot_gui.py
+++ noiseplot_generator/plot_gui.py
@@ -28,11 +28,7 @@ class InputFrame(ttk.Frame): # Make frame containing all input parameters
         self.input_frame = ttk.Frame() # Make frames
         self.fig_frame   = ttk.Frame()
 
-        self.sv_lab     = tk.Label(self.input_frame, text = "Filename:") # Make widgets
-        self.sv_ent     = tk.Entry(self.input_frame, width = 14)
-        self.sv_btn     = tk.Button(self.input_frame, command = self.set_sv_nm,
-                                    text = "Browse", width = 4, height = 0)
-        self.pwr_lab    = tk.Label(self.input_frame, text = "Power setting 1:")
+        self.pwr_lab    = tk.Label(self.input_frame, text = "Power setting 1:") # Make widgets
         self.pwr_ent    = tk.Entry(self.input_frame, width = 10)
         self.pwr_unit   = tk.Label(self.input_frame, text = "Power units")
         self.pwr_lab_2  = tk.Label(self.input_frame, text = "Power setting 2:")
@@ -44,10 +40,15 @@ class InputFrame(ttk.Frame): # Make frame containing all input parameters
         self.ac_drp     = ttk.OptionMenu(self.input_frame, self.ac, *["Select AC"],
                                          *fn.list_aircraft(), command = self.set_info)
         self.plt_btn    = tk.Button(self.input_frame, command = self.show_plot, 
-                                    text = "Plot", width = 4, height = 0)
+                                    text = "Preview plot", height = 0)
+        self.csv_btn    = tk.Button(self.input_frame, command = self.save_data,
+                                    text = "Save CSV data")
+        self.sv_btn     = tk.Button(self.input_frame, command = self.save_plot,
+                                    text = "Save plot", height = 0)
+        self.help_btn   = tk.Button(self.input_frame, command = self.show_help,
+                                    text = "Show help", height = 0)
 
-        self.sv_ent["textvariable"]     = self.sv_nm # Link widgets to variables
-        self.pwr_ent["textvariable"]    = self.pwr
+        self.pwr_ent["textvariable"]    = self.pwr # Link widgets to variables
         self.pwr_ent_2["textvariable"]  = self.pwr_2
         self.pwr_unit["textvariable"]   = self.units
         self.pwr_unit_2["textvariable"] = self.units
@@ -63,46 +64,89 @@ class InputFrame(ttk.Frame): # Make frame containing all input parameters
         self.pwr_unit_2.grid(row  = 2, column = 2, sticky = "W")
         self.desc_lab.grid(row    = 3, column = 0, sticky = "E")
         self.desc_ent.grid(row    = 3, column = 1, sticky = "WE")
-        self.sv_lab.grid(row      = 4, column = 0, sticky = "E")
-        self.sv_ent.grid(row      = 4, column = 1, sticky = "WE")
-        self.sv_btn.grid(row      = 4, column = 2,)
-        self.plt_btn.grid(row     = 5, column = 1, sticky = "W")
-        self.input_frame.grid(row = 0, column = 0) # Frame geometry
+        self.plt_btn.grid(row     = 4, column = 0, sticky = "WE")
+        self.sv_btn.grid(row      = 4, column = 1, sticky = "WE")
+        self.csv_btn.grid(row     = 5, column = 0, sticky = "WE")
+        self.help_btn.grid(row    = 5, column = 1, sticky = "WE")
+        self.input_frame.grid(row = 0, column = 0) # Geometry of frames
         self.fig_frame.grid(row   = 1, column = 0)
 
-    def make_plot(self): # make a dataframe, call plotting function 
+    def make_plot(self, save_name = None): # make a dataframe, call plotting function 
         self.df = interpolate(ac = self.ac.get(), pwr = float(self.pwr.get()),
                               units = self.units.get(), eng = self.eng.get())
         self.df_2 = None if self.pwr_2.get().isnumeric() == False else \
                   interpolate(ac = self.ac.get(), pwr = float(self.pwr_2.get()),
                               units = self.units.get(), eng = self.eng.get())
         try: # Try to make a figure from dataframes
-            self.fig = nl.plot(self.df, save_name = self.sv_nm.get()) \
+            self.fig = nl.plot(self.df, ps_name = self.desc.get(), 
+                                save_name = save_name) \
                 if self.df_2 is None else \
-                nl.plot(self.df, self.df_2, save_name = self.sv_nm.get(),
-                        ps_name = self.desc.get())
+                nl.plot(self.df, self.df_2, ps_name = self.desc.get(), 
+                                save_name = save_name)
             return(self.fig)
-        except: # Show error if dataframes are out of range
+        except: # Show error if power settings are out of range
             err = self.df if type(self.df) == str else self.df_2
-            self.show_error("Out of range", err)
+            self.show_message("Out of range", err)
             raise Exception("Could not make a plot out of those power settings. Perhaps they were out of range?") 
 
-    def show_plot(self):
-        fig = self.make_plot()
+    def show_plot(self): # Preview the plot on canvas
+        fig = self.make_plot(save_name = None)
         self.canvas = FigureCanvasTkAgg(fig, master = self.fig_frame)
         self.canvas.draw()
         self.canvas.get_tk_widget().grid(row = 1, column = 0)
-    
-    def set_sv_nm(self): # Open save dialog, set sv_nm/s_ent to selected name
-        nm = fd.asksaveasfilename()
-        self.sv_nm.set(nm)
+
+    def save_plot(self): # Save plot
+            self.fig = self.make_plot(save_name = fd.asksaveasfilename())
+ 
+    def save_data(self): # make a dataframe, save as csv 
+        self.df = interpolate(ac = self.ac.get(), pwr = float(self.pwr.get()),
+                              units = self.units.get(), eng = self.eng.get(),
+                              desc = self.desc.get())
+        self.df_2 = None if self.pwr_2.get().isnumeric() == False else \
+                  interpolate(ac = self.ac.get(), pwr = float(self.pwr_2.get()),
+                              units = self.units.get(), eng = self.eng.get(),
+                              desc = self.desc.get())
+        if type(self.df) != str and type(self.df_2) == str: # Try to save dataframes
+            self.df.to_csv(fd.asksaveasfilename())
+        elif type(self.df) != str and type(self.df_2) != str:
+             file = fd.asksaveasfilename()
+             self.df.to_csv(file, mode = 'w', index = False)
+             self.df_2.to_csv(file, mode = 'a', index = False, header = False)
+        else: 
+            err = self.df if type(self.df) == str else self.df_2 # Show message if error
+            self.show_message("Out of range", err)
+            raise Exception("Could not make a plot out of those power settings. Perhaps they were out of range?")    
     
     def set_info(self, selected): # Get units and engine of selected aircraft
         self.units.set(fn.get_info(selected)["units"])
         self.eng.set(fn.get_info(selected)["engine"])
     
-    def show_error(self, header, message):
+    def show_message(self, header, message):
         tkinter.messagebox.showinfo(header, message)
+
+    def show_help(self):
+        msg = \
+        '''
+Instructions:\n
+1.    Select aircraft from the drop-down.\n
+2.    Enter power setting values.
+          - To plot a single power setting,
+            leave "Power setting 2" blank.
+          - To plot a range between two power
+            settings, enter a value for
+            "Power setting 2".\n
+4.    Enter the name of the power setting in
+       the "Power description" field.\n
+5.    Show the plot in the window using
+       the "Preview plot" button.\n
+6.    Save the plot to a specified file using
+       the "Save plot" button.\n
+7.    Save the raw data file in csv format
+       to a specified location using the 
+       "Save CSV data" button.
+
+        '''
+        self.show_message("Help", msg)
 
 root = tk.Tk()
 a = InputFrame(root)
